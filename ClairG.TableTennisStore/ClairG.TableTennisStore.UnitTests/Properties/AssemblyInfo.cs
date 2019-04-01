5bf74e403a6fb556ef102006b4c16a685b76f4c2
lemon <lemon>
2019-04-01 12:06:06 -0400

    1.[NEW] add new solution;
    2.[NEW] add WebApp project;
    3.[NEW] add Domain project;
    4.[NEW] add UnitTest project;
    5.[CHG] add references;
    6.[CHG] update nuget;

---
+++ ClairG.TableTennisStore/ClairG.TableTennisStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClairG.TableTennisStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClairG.TableTennisStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f19fcda4-894b-4443-8cf5-843b9a6054d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
