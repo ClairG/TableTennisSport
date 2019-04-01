9bb96ec11789ba6ee5e3e7c113c72301423230f6
lemon <lemon>
2019-04-01 12:20:53 -0400

    1.[CHG] add IProductsRepository.cs in Domain-Abstract

---
+++ ClairG.TableTennisStore/ClairG.TableTennisStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClairG.TableTennisStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClairG.TableTennisStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
